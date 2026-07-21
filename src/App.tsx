313673c652ea352b13cda34899f40b220297cadd
Flor <florazul.fb@gmail.com>
2026-07-21 20:57:13 +0200

    add listing information + add edit button

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <>
-      <header className="bg-lime-600 py-3">
+      <header className="bg-[#787569] 0 py-3">
         <div className="max-w-4xl mx-auto flex justify-between">
           <h1 className="text-center text-lg font-bold text-white uppercase">
             Contador de Calorias
@@ -16,7 +16,7 @@ function App() {
         </div>
       </header>
 
-      <section className="bg-lime-300 py-20 px-5">
+      <section className="bg-[#C9C5B1] py-20 px-5">
         <div className="max-w-4xl mx-auto">
           <Form dispatch={dispatch} />
         </div>
